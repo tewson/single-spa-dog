42008c1e918077b3cd4f4fecc976b2d79a410948
Tewson Seeoun <tewson.seeoun@toasttab.com>
2019-07-01 16:03:01 +0100

    Add home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
 import { FeedComponent } from './feed/feed.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'feed', component: FeedComponent },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StatusComponent } from './status/status.component';
 import { FeedComponent } from './feed/feed.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StatusComponent,
-    FeedComponent
+    FeedComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
